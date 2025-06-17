1c368ca8057de61a43814e4a03e73e0614f746c6
ehceyn <itzmeruz31@gmail.com>
2025-06-17 16:26:58 +0100

    fix: correct syntax and improve import structure in ThemeProvider component

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,16 +1,15 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import * as React from "react";
+import {
+  ThemeProvider as NextThemesProvider,
+  type ThemeProviderProps as NextThemesProviderProps,
+} from "next-themes";
 
-interface ThemeProviderProps {
-  children: React.ReactNode
-  attribute?: string
-  defaultTheme?: string
-  enableSystem?: boolean
-  disableTransitionOnChange?: boolean
+interface ThemeProviderProps extends NextThemesProviderProps {
+  children: React.ReactNode;
 }
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
